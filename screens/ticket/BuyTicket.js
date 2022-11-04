840221be0ee2565778c97b0d0e613395709602a0
tharaka dilshan <88264479+tharaka-99@users.noreply.github.com>
2022-11-04 10:03:27 +0530

    BuyTickets page changers

--- screens/ticket/BuyTicket.js
+++ screens/ticket/BuyTicket.js
@@ -1,4 +1,10 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+} from "react-native";
 import React, { useState, useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { db } from "../../Firebase/Firebase-config";
@@ -6,61 +12,120 @@ import { collection, getDocs, doc, deleteDoc } from "firebase/firestore";
 
 export default function BuyTicket() {
   const DatCollectinRef = collection(db, "Package");
-  const [getData, setGetData] = useState("");
-  const [data, setData] = useState("");
+  const [travelPackage, setTravelPackage] = useState("");
 
   useEffect(() => {
     //fetch the all data from firebase and set it to usestate, this firebase method
     const getAllData = async () => {
       const data = await getDocs(DatCollectinRef);
-      setGetData(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setTravelPackage(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
     };
 
     getAllData();
   }, []);
-  // console.log(getData);
-
-  const getCurrentDate = () => {
-    var result = new Date();
-    result.setDate(30);
-    result.setHours(result.getHours() + 12);
-    var date = result.getDate();
-    var month = result.getMonth() + 1;
-    var year = result.getFullYear();
-    var hour = result.getHours();
-    var minites = result.getMinutes();
-    var seconds = result.getSeconds();
-
-    //Alert.alert(date + '-' + month + '-' + year);
-    // You can turn it in to your desired format
-    return (
-      date +
-      "-" +
-      month +
-      "-" +
-      year +
-      "/---" +
-      hour +
-      ":" +
-      minites +
-      ":" +
-      seconds
-    ); //format: d-m-y;
-  };
+  // console.log(travelPackage);
 
   return (
-    <View style={{ margin: 10 }}>
-      <View
+    <View style={{ backgroundColor: "#FFF", marginBottom: "20%" }}>
+      <Text
         style={{
-          backgroundColor: "#fff",
-          elevation: 10,
-          height: 50,
-          borderRadius: 10,
-          padding: 5,
+          textAlign: "center",
+          margin: 13,
+          fontSize: 23,
+          fontWeight: "900",
         }}
       >
-        <Text>{getCurrentDate()}</Text>
-      </View>
+        Travel Packages
+      </Text>
+      <FlatList
+        data={travelPackage}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              backgroundColor: "#FFFDE7",
+              elevation: 10,
+              borderRadius: 10,
+              marginVertical: 10,
+              marginHorizontal: 15,
+            }}
+          >
+            <View
+              style={{
+                padding: 10,
+              }}
+            >
+              <Text
+                style={{
+                  marginLeft: "10%",
+                  fontSize: 20,
+                  fontWeight: "bold",
+                }}
+              >
+                {item.package_name}
+              </Text>
+
+              <View
+                style={{
+                  borderTopWidth: 1,
+                  borderColor: "#B0BEC5",
+                  flexDirection: "row",
+                  justifyContent: "space-between",
+                  marginTop: 10,
+                  paddingTop: 10,
+                }}
+              >
+                <View
+                  style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                  }}
+                >
+                  <Text style={{ fontSize: 17, opacity: 0.5 }}>Valid for</Text>
+                  <View
+                    style={{
+                      backgroundColor: "#FFD54F",
+                      marginLeft: 10,
+                      padding: 4,
+                      borderRadius: 5,
+                    }}
+                  >
+                    <Text style={{ fontSize: 16 }}>{item.valid} Days</Text>
+                  </View>
+                </View>
+                <View>
+                  <Text
+                    style={{
+                      fontSize: 25,
+                      fontWeight: "bold",
+                    }}
+                  >
+                    Rs {item.price}
+                  </Text>
+                </View>
+              </View>
+            </View>
+            <TouchableOpacity
+              style={{
+                flexDirection: "row",
+                justifyContent: "center",
+                backgroundColor: "#FFB200",
+                borderBottomEndRadius: 10,
+                borderBottomLeftRadius: 10,
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: 20,
+                  fontWeight: "bold",
+                  marginVertical: 5,
+                }}
+              >
+                Get it
+              </Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      />
     </View>
   );
 }
